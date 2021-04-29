c6f3a705078b5b9d8c9e31f2b2fdd65cf7354957
Talia-codes <80581556+Talia-codes@users.noreply.github.com>
2021-04-29 20:31:21 +0200

    Connected to API to populate with forecast weather data

--- src/script.js
+++ src/script.js
@@ -86,32 +86,45 @@ let formattedDate = `${day} ${hours}:${minutes}`;
 let h2 = document.querySelector("#date");
 h2.innerHTML = formatDate(new Date());
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
 
   <div class="col d-flex justify-content-center">
   <div class="day-background1">
-  <div class="forecast-day">${day}</div>
-<img src="http://openweathermap.org/img/wn/50d@2x.png"
+  <div class="forecast-day">${formatDay(forecastDay.dt)}</div>
+
+<img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
 width="30"/>
   <div class="forecast-temperature-max">
-  21°
+  ${Math.round(forecastDay.temp.max)}°
   </div>
   <div class="forecast-temperature-min">
-  9°
+  ${Math.round(forecastDay.temp.min)}°
   </div>
   </div>
   </div>
 
   `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
